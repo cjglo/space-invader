6ea9cda823a2514079cd5a5fba0cb0864c877875
cjglo <chris.j.gallo3@gmail.com>
2022-01-20 23:06:27 -0500

    sprite animates on continuous timer

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,11 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
         .add_system(movement::player_movement)
+        .add_system(movement::sprite_animation_system)
         .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut texture_atlases: ResMut<Assets<TextureAtlas>>) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
    
     commands.spawn_bundle(SpriteBundle {
@@ -21,15 +22,18 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         transform: Transform::from_scale(Vec3::new(2.3, 2.3, 0.0)),
         ..Default::default()
     });
-
-    commands.spawn_bundle(SpriteBundle {
-        texture: asset_server.load("player.png"),
+    let texture_handle = asset_server.load("playerspr.png");
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(31.0,32.0), 3, 4);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    commands.spawn_bundle(SpriteSheetBundle {
+        texture_atlas: texture_atlas_handle,
         transform: Transform {
-                scale: Vec3::new(0.07, 0.07, 0.0),
+                scale: Vec3::new(1.1, 1.1, 0.0),
                 translation: Vec3::new(0.0, 0.0, 1.0),
                 ..Default::default()
             }, 
         ..Default::default()
     })
+    .insert(Timer::from_seconds(0.1,true))
     .insert(movement::Movement { speed: 9.0});
 }
